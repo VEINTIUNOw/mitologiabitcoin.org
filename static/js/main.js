e2496a4e9b5a48532c068ffeb4e1462fb2ee4c66
Raj <varasaniraj5@gmail.com>
2023-02-24 20:10:05 +0530

    Revert "update vercel analytics"
    This reverts commit da3fee09e410c05c7e0b80628fbf9393fb62d353.

--- static/js/main.js
+++ static/js/main.js
@@ -1,9 +1,4 @@
-console.log('This Works')
-document.addEventListener('DOMContentLoaded', () => {
-  //   import { inject } from '@vercel/analytics'
-  // inject()
-  console.log('This Works too')
-  const inject = require('@vercel/analytics').inject
-  console.log('inject', inject)
-  inject()
-})
+import { inject } from '@vercel/analytics'
+inject()
+
+document.addEventListener('DOMContentLoaded', () => {})
