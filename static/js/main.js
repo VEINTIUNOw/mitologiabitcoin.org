da3fee09e410c05c7e0b80628fbf9393fb62d353
Raj <varasaniraj5@gmail.com>
2023-02-24 19:39:15 +0530

    update vercel analytics

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,9 @@
-import { inject } from '@vercel/analytics'
-inject()
-
-document.addEventListener('DOMContentLoaded', () => {})
+console.log('This Works')
+document.addEventListener('DOMContentLoaded', () => {
+  //   import { inject } from '@vercel/analytics'
+  // inject()
+  console.log('This Works too')
+  const inject = require('@vercel/analytics').inject
+  console.log('inject', inject)
+  inject()
+})
